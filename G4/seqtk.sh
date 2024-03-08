2ec3163c77fd40908a11cd1e61f6b9298a55e1d0
Natalia Zajac <natalia.zajac@fgcz.ethz.ch>
2024-03-08 16:31:45 +0100

    seqtk in G4 fix

--- G4/seqtk.sh
+++ G4/seqtk.sh
@@ -14,7 +14,7 @@ module load Tools/seqtk/1.3
 DIR=/srv/GT/analysis/zajacn/p2220/o31444_G4/G4_p2220_o31444_subsampled_v2
 
 k=$SLURM_ARRAY_TASK_ID
-name=`sed -n ${k}p < /srv/GT/analysis/zajacn/p2220/o31444_G4/G4_p2220_o31444_subsampled_v2/list_of_files`
+name=`sed -n ${k}p < $(ls /srv/gstore/projects/p2220/G4_FC4_1S11M7B_20230715/*R1_001.fastq.gz)`
 
-seqtk sample -s100 /srv/gstore/projects/p2220/G4_FC4_1S11M7B_20230715/${name} 2000000 > $DIR/${name}.subsampled.fastq
-gzip $DIR/${name}.subsampled.fastq
+seqtk sample -s100 /srv/gstore/projects/p2220/G4_FC4_1S11M7B_20230715/${name} 2000000 | gzip > $DIR/${name}.subsampled.fastq
+
